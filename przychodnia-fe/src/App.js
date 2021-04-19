197052ae41547cf7fd8032abda67c083fa5d1b53
Wiktor Gacek <darono87@gmail.com>
2021-04-19 22:43:58 +0200

    feat: Add Dependencies, example with Ant Design

---
+++ przychodnia-fe/src/App.js
@@ -0,0 +1,30 @@
+import { Card } from 'antd'
+import logo from './logo.svg';
+import './App.css';
+import 'antd/dist/antd.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Card title="Default size card" extra={<a href="#">More</a>} style={{ width: 300 }}>
+          <p>Hello ANT DESIGN!</p>
+        </Card>
+      </header>
+    </div>
+  );
+}
+
+export default App;
